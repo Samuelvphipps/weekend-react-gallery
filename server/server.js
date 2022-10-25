9d9d0032acf7dde6178ca885fab608f55ec65905
Sam Phipps <samuel.v.phipps@gmail.com>
2022-10-25 16:17:00 -0500

    changed port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const gallery = require('./routes/gallery.router.js');
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
